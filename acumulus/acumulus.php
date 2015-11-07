e1d5dd4b3e271653c7883644c2ff8463afddffe7
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2015-11-07 18:12:14 +0100

    4.1.1: updated filters.txt

--- acumulus/acumulus.php
+++ acumulus/acumulus.php
@@ -4,7 +4,7 @@ Plugin Name: Acumulus
 Description: Acumulus koppeling voor WooCommerce 2.3+
 Plugin URI: https://forum.acumulus.nl/index.php?board=17.0
 Author: Acumulus
-Version: 4.0.2
+Version: 4.1.1
 LICENCE: GPLv3
 */
 
