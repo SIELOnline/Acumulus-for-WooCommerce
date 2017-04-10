bfbd9930d7cc21f22100aabd9debf3d8921160e1
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2017-04-10 21:42:41 +0200

    svn release commando's in batch gezet

---
+++ release.bat
@@ -0,0 +1,14 @@
+@echo off
+rem Check usage and arguments.
+if dummy==dummy%1 (
+echo Usage: %~n0 version
+exit /B 1;
+)
+set version=%1
+
+@echo on
+call copy-wc-svn
+cd svn
+svn add --force * --auto-props --parents --depth infinity
+svn cp trunk tags/%version%
+svn ci --username "SIEL Acumulus" --password yddez(9tCZYT -m "%version%"
