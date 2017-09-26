4ccd363ded5e8e0f2ac42e716fc1b2c9f433bd0e
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2017-09-26 14:00:25 +0200

    4.9.1

--- acumulus/acumulus.php
+++ acumulus/acumulus.php
@@ -4,7 +4,7 @@ Plugin Name: Acumulus
 Description: Acumulus plugin for WooCommerce 2.4+
 Plugin URI: https://wordpress.org/plugins/acumulus/
 Author: SIEL Acumulus
-Version: 4.8.0
+Version: 4.9.1
 LICENCE: GPLv3
 */
 
