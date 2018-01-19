a16dc71a62cfca93156cbe6576d482fcac769fe1
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2018-01-19 20:46:24 +0100

    release batch

--- release.bat
+++ release.bat
@@ -5,11 +5,14 @@ echo Usage: %~n0 version
 exit /B 1;
 )
 set version=%1
+set repository=svn
 
 @echo on
 call copy-wc-svn
-cd svn
-svn add --force * --auto-props --parents --depth infinity
+cd %repository%
+cd trunk
+svn add --force . --auto-props --parents --depth infinity
+cd ..
 svn cp trunk tags/%version%
 svn ci --username "SIEL Acumulus" --password yddez(9tCZYT -m "%version%"
 cd ..
