18c3a19b68666a0888aac35f8a04b5964690699b
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2021-01-16 21:46:58 +0100

    6.1.2

--- acumulus.php
+++ acumulus.php
@@ -4,13 +4,13 @@
  * Description: Acumulus plugin for WooCommerce
  * Author: Buro RaDer, https://burorader.com/
  * Copyright: SIEL BV, https://www.siel.nl/acumulus/
- * Version: 6.1.0
+ * Version: 6.1.2
  * LICENCE: GPLv3
  * Requires at least: 4.2.3
  * Tested up to: 5.6
  * WC requires at least: 3.0
  * WC tested up to: 4.8
- * libAcumulus requires at least: 6.1.0
+ * libAcumulus requires at least: 6.1.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
