91b0ee195fd6c302a11ac82ce31dbc3a0763f8b2
Erwin Derksen <erwin@burorader.com>
2023-09-28 12:28:23 +0200

    Moved test order getting and saving into our AcumulusTestUtils trait

--- tests/WooCommerce/Integration/InvoiceCreateTest.php
+++ tests/WooCommerce/Integration/InvoiceCreateTest.php
@@ -10,7 +10,8 @@ namespace Siel\Acumulus\Tests\WooCommerce\Integration;
 use Siel\Acumulus\Fld;
 use Siel\Acumulus\Invoice\Source;
 use Siel\Acumulus\Tests\WooCommerce\Acumulus_WooCommerce_TestCase;
-use Siel\Acumulus\Tests\WooCommerce\Data\TestData;
+
+use function dirname;
 
 /**
  * InvoiceCreateTest tests the process of creating an {@see Invoice}.
@@ -19,15 +20,16 @@ class InvoiceCreateTest extends Acumulus_WooCommerce_TestCase
 {
     public function InvoiceDataProvider(): array
     {
+        $dataPath = dirname(__FILE__, 2) . '/Data';
         return [
-            'NL consument' => [Source::Order, 61,],
-            'NL company' => [Source::Order, 62,],
-            '1 fixed amount coupon' => [Source::Order, 67,],
-            '2 (fixed amount and percentage) coupons' => [Source::Order, 68,],
-            'reversed vat, different shipping country, variants' => [Source::Order, 69,],
-            'EU VAT, variants, percentage coupon' => [Source::Order, 70,],
-            'EU VAT Belgium (same vat rate as NL), shipping to NL' => [Source::Order, 71,],
-            'NL Refund' => [Source::CreditNote, 73,],
+            'NL consument' => [$dataPath, Source::Order, 61,],
+            'NL company' => [$dataPath, Source::Order, 62,],
+            '1 fixed amount coupon' => [$dataPath, Source::Order, 67,],
+            '2 (fixed amount and percentage) coupons' => [$dataPath, Source::Order, 68,],
+            'reversed vat, different shipping country, variants' => [$dataPath, Source::Order, 69,],
+            'EU VAT, variants, percentage coupon' => [$dataPath, Source::Order, 70,],
+            'EU VAT Belgium (same vat rate as NL), shipping to NL' => [$dataPath, Source::Order, 71,],
+            'NL Refund' => [$dataPath, Source::CreditNote, 73,],
         ];
     }
 
@@ -38,25 +40,8 @@ class InvoiceCreateTest extends Acumulus_WooCommerce_TestCase
      * @dataProvider InvoiceDataProvider
      * @throws \JsonException
      */
-    public function testCreate(string $type, int $id, array $excludeFields = []): void
+    public function testCreate(string $dataPath, string $type, int $id, array $excludeFields = []): void
     {
-        $invoiceSource = $this->getAcumulusContainer()->createSource($type, $id);
-        $invoiceAddResult = $this->getAcumulusContainer()->createInvoiceAddResult('SendInvoiceTest::testCreateAndCompleteInvoice()');
-        $invoice = $this->getAcumulusContainer()->getInvoiceCreate()->create($invoiceSource, $invoiceAddResult);
-        $result = $invoice->toArray();
-        $testData = new TestData();
-        // Get order from Order{id}.json.
-        $expected = $testData->get($type, $id);
-        if ($expected !== null) {
-            // Save order to Order{id}.latest.json, so we can compare differences ourselves.
-            $testData->save($type, $id, false, $result);
-            $this->assertCount(1, $result);
-            $this->assertArrayHasKey(Fld::Customer, $result);
-            $this->compareAcumulusObjects($expected[Fld::Customer], $result[Fld::Customer], Fld::Customer, $excludeFields);
-        } else {
-            // File does not yet exist: first time for a new test order: save order to Order{id}.json.
-            // Will raise a warning that no asserts have been executed.
-            $testData->save($type, $id, true, $result);
-        }
+        $this->_testCreate($dataPath, $type, $id, $excludeFields);
     }
 }
