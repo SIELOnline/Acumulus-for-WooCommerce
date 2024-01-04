547dc8108a96b5c05adaa7996ce271355823721f
Erwin Derksen <erwin@burorader.com>
2024-01-04 22:06:04 +0100

    8.1.1

--- acumulus.php
+++ acumulus.php
@@ -4,13 +4,13 @@
  * Description: Acumulus plugin for WooCommerce
  * Author: Buro RaDer, https://burorader.com/
  * Copyright: SIEL BV, https://www.siel.nl/acumulus/
- * Version: 8.1.0
+ * Version: 8.1.1
  * LICENCE: GPLv3
  * Requires at least: 5.9
  * Tested up to: 6.4
  * WC requires at least: 5.0
  * WC tested up to: 8.4
- * libAcumulus requires at least: 8.1.0
+ * libAcumulus requires at least: 8.1.1
  * Requires PHP: 7.4
  */
 
